85a86cd3abd751f7e4dd6ef28aea51408b2b8856
Sammy99jsp <sammy99jsp@gmail.com>
2021-07-02 13:30:32 +0100

    background colour black -- not truw lol

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -83,26 +83,110 @@ const OPCODES = [
         flags: "----"
     },
     {
-        opcode: "2A",
-        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "A"}, {type: "reference", value: [{type: "register", value: "HL"}, {type: "operator", value: "++"}]}],
-        description: "Load the value of memory at address HL, then increment HL.",
+        opcode: "02",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "reference", value: [{type: "register", value: "BC"}]}, {type: "register", value: "A"}],
+        description: "Loads the value of A into memory at address BC.",
         cycles: 8,
         flags: "----"
     },
     {
-        opcode: "F0",
-        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "A"}, {type: "reference", value: [{type: "constant", value: "FF00"}, {type: "operator", value: "+"}, {type: "register", value: "A"}]}],
-        description: "Load the value of ($FF00 + A) into A.",
-        cycles: 12,
+        opcode: "03",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "BC"}],
+        description: "Increment the BC register pair.",
+        cycles: 8,
         flags: "----"
     },
     {
-        opcode: "CB 37",
-        syntax: [{type: "mnemonic", value: "SWAP"}, {type: "register", value: "A"}],
-        description: "Swap upper and lower nibbles of A.",
+        opcode: "04",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "B"}],
+        description: "Increment the B register.",
+        cycles: 4,
+        flags: "CRC-"
+    },
+    {
+        opcode: "05",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "B"}],
+        description: "Decrement the B register.",
+        cycles: 4,
+        flags: "CSC-"
+    },
+    {
+        opcode: "06",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "B"}, {type: "variable", value: "n"}],
+        description: "Loads an 8-bit value into register B.",
         cycles: 8,
-        flags: "CRRR"
-    }
+        flags: "----"
+    },
+    {
+        opcode: "07",
+        syntax: [{type: "mnemonic", value: "RLC"}, {type: "register", value: "A"}],
+        description: "Rotate A to the left. And put the old bit 7 into the Carry flag.",
+        cycles: 4,
+        flags: "CRRC"
+    },
+    {
+        opcode: "08",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "reference", value: [{type: "variable", value: "nn"}]}, {type: "register", value: "SP"}],
+        description: "Set the Stack Pointer to address nn. ",
+        cycles: 20,
+        flags: "----"
+    },
+    {
+        opcode: "09",
+        syntax: [{type: "mnemonic", value: "ADD"}, {type: "register", value: "HL"}, {type: "register", value: "BC"}],
+        description: "Add BC to HL.",
+        cycles: 8,
+        flags: "-RCC"
+    },
+    {
+        opcode: "0A",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "A"}, {type: "reference", value: [{type: "register", value: "BC"}]}],
+        description: "Put value at address BC into A.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "0B",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "BC"}],
+        description: "Decrement the BC register pair.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "0C",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "C"}],
+        description: "Increment the C register.",
+        cycles: 4,
+        flags: "CRC-"
+    },
+    {
+        opcode: "0D",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "C"}],
+        description: "Decrement the C register.",
+        cycles: 4,
+        flags: "CSC-"
+    },
+    {
+        opcode: "0E",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "C"}, {type: "variable", value: "n"}],
+        description: "Put value n into C.",
+        cycles: 4,
+        flags: "----"
+    },
+    {
+        opcode: "0F",
+        syntax: [{type: "mnemonic", value: "RRC"}, {type: "register", value: "A"}],
+        description: "Rotate A right and put the old 0 into the Carry flag.",
+        cycles: 4,
+        flags: "CRRC"
+    },
+    {
+        opcode: "10 00",
+        syntax: [{type: "mnemonic", value: "STOP"}],
+        description: "Halt the CPU & LCD display until a button is pressed. .",
+        cycles: 4,
+        flags: "----"
+    },
 ];
 
 OPCODES.forEach(opcode => {
