77a404ef164eb066f1246f63edc4dd89c6561192
Sammy99jsp <sammy99jsp@gmail.com>
2021-07-03 23:28:12 +0100

    Added all opcodes (scrapped from site).

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,3 +1,9 @@
+
+
+
+$('.container').on("scroll", function(e){
+    localStorage.setItem("scroll-pos", $('.container').scrollTop());
+});
 function htmlForInstruction({opcode, syntax, description, flags, cycles}) {
     const [Z, N, H, C] = ([...(flags || [])]).map(flagParse);
     return `<div class="card">
@@ -67,7 +73,7 @@ function flagParse(flag) {
     operator — A maths operator like + - * /
 */
 
-const OPCODES = [
+/*const OPCODES = [
     {
         opcode: "00",
         syntax: [{type: "mnemonic", value: "NOP"}],
@@ -183,12 +189,114 @@ const OPCODES = [
     {
         opcode: "10 00",
         syntax: [{type: "mnemonic", value: "STOP"}],
-        description: "Halt the CPU & LCD display until a button is pressed. .",
+        description: "Halt the CPU & LCD display until a button is pressed.",
         cycles: 4,
         flags: "----"
     },
-];
+    {
+        opcode: "11",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "DE"}, {type: "variable", value: "nn"}],
+        description: "Load a 16-bit value into the DE register pair.",
+        cycles: 12,
+        flags: "----"
+    },
+    {
+        opcode: "12",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "reference", value: [{type: "register", value: "DE"}]}, {type: "register", value: "A"}],
+        description: "Put the value of A into address (DE)",
+        cycles: 8
+    },
+    {
+        opcode: "13",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "DE"}],
+        description: "Increments the value of register pair DE.",
+        cycles: 8
+    },
+    {
+        opcode: "14",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "D"}],
+        description: "Increments the value of the D register.",
+        cycles: 4,
+        flags: "CRC-"
+    },
+    {
+        opcode: "15",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "D"}],
+        description: "Decrements the value of the D register.",
+        cycles: 4,
+        flags: "CSC-"
+    },
+    {
+        opcode: "16",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "variable", value: "n"}],
+        description: "Load an 8-bit value into the D register.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "17",
+        syntax: [{type: "mnemonic", value: "RL"}, {type: "register", value: "A"}],
+        description: "Rotate register A left through the Carry flag.",
+        cycles: 4,
+        flags: "CRRC"
+    },
+    {
+        opcode: "18",
+        syntax: [{type: "mnemonic", value: "JR"}, {type: "register", value: "r"}],
+        description: "Add r to the current instruction address and jump to it.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "19",
+        syntax: [{type: "mnemonic", value: "ADD"}, {type: "register", value: "HL"}, {type: "register", value: "DE"}],
+        description: "Add DE to HL.",
+        cycles: 8,
+        flags: "-RCC"
+    },
+    {
+        opcode: "1A",
+        syntax: [{type: "mnemonic", value: "LD"}, {type: "register", value: "A"}, {type: "reference", value: [{type: "register", value: "DE"}]}],
+        description: "Load the value at address DE into A.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "1B",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "DE"}],
+        description: "Decrement the value in the DE register pair.",
+        cycles: 8,
+        flags: "----"
+    },
+    {
+        opcode: "1C",
+        syntax: [{type: "mnemonic", value: "INC"}, {type: "register", value: "E"}],
+        description: "Increment the value of the E register.",
+        cycles: 4,
+        flags: "CRC-"
+    },
+    {
+        opcode: "1D",
+        syntax: [{type: "mnemonic", value: "DEC"}, {type: "register", value: "E"}],
+        description: "Decrement the value of the E register.",
+        cycles: 4,
+        flags: "CSC-"
+    }
+];*/
 
-OPCODES.forEach(opcode => {
-    $('#opcodes').append(htmlForInstruction(opcode));
+window.instructions.sort(function(a, b) {
+    console.log(a.opcode, b.opcode)
+    const [A, B] = [a.opcode.split(" ").map(e => parseInt(e, 16)), b.opcode.split(" ").map(e => parseInt(e, 16))];
+    return A[0] - B[0] === 0 ? A[1] - B[1] : A[0] - B[0];
+}).forEach(opcode => {
+    try {
+        $('#opcodes').append(htmlForInstruction(opcode));
+        
+    } catch (error) {
+        console.log(error);   
+    }
 })
+
+if(localStorage.getItem("scroll-pos")) {
+    $('.container').scrollTop(localStorage.getItem("scroll-pos"));
+}
